fae73ac408ef16c8ee13e831f6bb4d67c31d03ca
20SB <subhabiswal20@gmail.com>
2023-09-17 23:32:03 +0530

    server side api kept, styling done for every page

--- assets/js/interview.js
+++ assets/js/interview.js
@@ -1,5 +1,4 @@
 $("#choose-company").on("submit", function (event) {
-    console.log("apply clicked");
     event.preventDefault();
     let selectDiv = '#for-' + $("#slct-by-cmpny").val();
     showInterviewOf(selectDiv);
@@ -9,15 +8,21 @@ $("#choose-company").on("submit", function (event) {
 let lastOpenedStored = localStorage.getItem("lastOpened");
 
 if(lastOpenedStored !== null ){
-    console.log("lastOpened: "+ lastOpenedStored);
     showInterviewOf(lastOpenedStored);
 }else{
-    console.log("nothing stored");
     var abc = "#for-all";
     showInterviewOf(abc);
 }
 
 function showInterviewOf(selectDiv){
+    let cName = selectDiv.substring(5);
+    if(cName !== "all") {
+        $("#c-name").html(`for <strong>${cName}</strong>`);
+    }
+    else{
+        $("#c-name").html('');
+    }
+    
     $(".interview-table").addClass( "disp-none" );
     $(selectDiv).removeClass( "disp-none" );
     localStorage.setItem("lastOpened", selectDiv);
